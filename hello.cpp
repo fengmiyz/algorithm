bf4113255cc1c21fabadcd89c5bc9b5f67b509bc
fengmiyz <2283567810@qq.com>
2024-06-05 20:48:16 +0800

    add nihao.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void show(int a) {
+	cout << "hello wordl" << endl;
+}
+int main() {
+	int demo = 100;
+	show(demo);
+
+}
+
---
+++ hihao.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int dmeo = 100;
+	for (int i = 0; i < 20; i++) {
+		for (int j = 20; j >= 20; i++) {
+			cout << demo[i][j] << endl;
+		}}
+
